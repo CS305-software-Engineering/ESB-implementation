76cb607cf05a3aa494b9cc92e66ab8a789e8df9b
sathwik-r <55237681+sathwik-r@users.noreply.github.com>
2021-03-27 14:14:28 +0530

    Update rapidapi.py

--- rapidapi.py
+++ rapidapi.py
@@ -12,7 +12,7 @@ if(index==1):
         'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com"
         }
 
-    conn.request("GET", "/weather?q=London%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html", headers=headers)
+    conn.request("GET", "/weather?q=Warangal&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html", headers=headers)
 
     res = conn.getresponse()
     data = res.read()
