70f631901272975788092af9d5cd657a204c232c
SoniGarima <2018csb1089@iitrpr.ac.in>
2021-04-02 12:49:42 +0530

    formatted files

--- app.py
+++ app.py
@@ -1,85 +1,106 @@
-from flask import Flask,render_template,url_for,flash,redirect,request,session ,jsonify,send_file,Response
-from flask_sqlalchemy import SQLAlchemy
-from rapidapi import str_rev_api,translate_api,weather_api,insta_api
-from sqlalchemy import create_engine
+from flask import Flask, render_template, url_for, flash, redirect, request, session, jsonify, send_file, Response
+# from flask_sqlalchemy import SQLAlchemy
+import pymysql
+import os
+from rapidapi import str_rev_api, translate_api, weather_api, insta_api
+# from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
 # Google Cloud SQL (change this accordingly)
-PASSWORD ="root"
-PUBLIC_IP_ADDRESS ="35.192.77.35"
-DBNAME ="ESB"
-PROJECT_ID ="warm-skill-309311"
-INSTANCE_NAME ="esb-implementation"
-  
+# PASSWORD ="root"
+# PUBLIC_IP_ADDRESS ="35.192.77.35"
+# DBNAME ="ESB"
+# PROJECT_ID ="warm-skill-309311"
+# INSTANCE_NAME ="esb-implementation"
+
 # configuration
-app.config["SECRET_KEY"] = "pzaegniyzfvphtliybzmclacmx"
-app.config["SQLALCHEMY_DATABASE_URI"]= f'mysql+mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
-engine = create_engine('mysql+mysqldb://root:root@35.192.77.35/ESB?unix_socket=/cloudsql/warm-skill-309311:us-central1:esb-implementation')
-db = SQLAlchemy(app)
+# app.config["SECRET_KEY"] = "pzaegniyzfvphtliybzmclacmx"
+# app.config["SQLALCHEMY_DATABASE_URI"]= f'mysql+mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}'
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
+# engine = create_engine('mysql+mysqldb://root:root@35.192.77.35/ESB?unix_socket=/cloudsql/warm-skill-309311:us-central1:esb-implementation')
+# db = SQLAlchemy(app)
+
+db_connection_name = 'warm-skill-309311:us-central1:esb-implementation'
+
+
+def open_connection():
+    unix_socket = '/cloudsql/{}'.format(db_connection_name)
+    conn1 = pymysql.connect(user='root', password='root', unix_socket=unix_socket, db='ESB', cursorclass=pymysql.cursors.DictCursor)
+    return conn1
+
 
 admin = {}
 admin["username"] = "admin"
 admin["password"] = "pass123"
 
-@app.route("/",methods=['GET','POST'])
+
+@app.route("/", methods=['GET', 'POST'])
 def welcome_admin():
     if request.method == 'POST':
         if request.form["username"] == admin["username"] and request.form["password"] == admin["password"]:
             return redirect(url_for('admin_dashboard'))
     return render_template("admin_login.html")
 
-@app.route("/admin",methods=['GET','POST'])
+
+@app.route("/admin", methods=['GET', 'POST'])
 def admin_dashboard():
     return render_template("admin_dashboard.html")
 
+
 @app.route("/user_login")
 def user_login():
-    
+
     return render_template("user_login.html")
+
 
 @app.route("/user_signup")
 def user_signup():
-    with engine.connect() as connection:
-        result = connection.execute("create table IF NOT EXISTS Users(Username text primary key,UserPassword text not null,UserRole text not null,UserPriority int not null)")
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        res = cursor.execute('create table IF NOT EXISTS Users(Username text primary key,UserPassword text not null,UserRole text not null,UserPriority int not null)')
     return render_template("user_signup.html")
-    
-@app.route("/string_reverse", methods=['GET','POST'])
+
+
+@app.route("/string_reverse", methods=['GET', 'POST'])
 def string_reverse():
     out = "Output will be shown here."
     if request.method == 'POST':
         string = request.form["string"]
         out = str_rev_api(string)
-        return render_template("string_reverse.html",out = out)
-    return render_template("string_reverse.html",out = out)
-    
-@app.route("/instagram", methods=['GET','POST'])
+        return render_template("string_reverse.html", out=out)
+    return render_template("string_reverse.html", out=out)
+
+
+@app.route("/instagram", methods=['GET', 'POST'])
 def instagram():
     out = "Output will be shown here."
     if request.method == 'POST':
         string = request.form["string"]
         out = insta_api(string)
-        return render_template("insta.html",out = out)
-    return render_template("insta.html",out = out)
-    
-@app.route("/weather", methods=['GET','POST'])
+        return render_template("insta.html", out=out)
+    return render_template("insta.html", out=out)
+
+
+@app.route("/weather", methods=['GET', 'POST'])
 def weather():
     out = "Output will be shown here."
     if request.method == 'POST':
         string = request.form["string"]
         out = weather_api(string)
-        return render_template("weather.html",out = out)
-    return render_template("weather.html",out = out)
+        return render_template("weather.html", out=out)
+    return render_template("weather.html", out=out)
 
-@app.route("/translator", methods=['GET','POST'])
+
+@app.route("/translator", methods=['GET', 'POST'])
 def translator():
     out = "Output will be shown here."
     if request.method == 'POST':
         string = request.form["string"]
         out = translate_api(string)
-        return render_template("detect.html",out = out)
-    return render_template("detect.html",out = out)
+        return render_template("detect.html", out=out)
+    return render_template("detect.html", out=out)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
--- rapidapi.py
+++ rapidapi.py
@@ -1,52 +1,44 @@
 import http.client
 
-index=3
+index = 3
 
 
 def str_rev_api(string):
     conn = http.client.HTTPSConnection("sonigarima.pythonanywhere.com")
-    ip = "/"+str(string)
+    ip = "/" + str(string)
     conn.request("GET", ip)
     res = conn.getresponse()
     data = res.read()
     return data.decode("utf-8")
 
+
 #current weather data
 def weather_api(city):
     conn = http.client.HTTPSConnection("community-open-weather-map.p.rapidapi.com")
-    headers = {
-        'x-rapidapi-key': "48a9d3acbemsh51e3d6835ad11bep1a253cjsne66f78646111",
-        'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com"
-        }
-    ip = "/weather?q="+str(city)+"%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html"
+    headers = {'x-rapidapi-key': "48a9d3acbemsh51e3d6835ad11bep1a253cjsne66f78646111", 'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com"}
+    ip = "/weather?q=" + str(city) + "%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html"
     conn.request("GET", ip, headers=headers)
     res = conn.getresponse()
     data = res.read()
     return data.decode("utf-8")
 
+
 #instagram account info
-def  insta_api(username):
+def insta_api(username):
     conn = http.client.HTTPSConnection("instagram40.p.rapidapi.com")
-    headers = {
-        'x-rapidapi-key': "48a9d3acbemsh51e3d6835ad11bep1a253cjsne66f78646111",
-        'x-rapidapi-host': "instagram40.p.rapidapi.com"
-        }
-    ip = "/account-info?username="+str(username)
+    headers = {'x-rapidapi-key': "48a9d3acbemsh51e3d6835ad11bep1a253cjsne66f78646111", 'x-rapidapi-host': "instagram40.p.rapidapi.com"}
+    ip = "/account-info?username=" + str(username)
     conn.request("GET", ip, headers=headers)
     res = conn.getresponse()
     data = res.read()
     return data.decode("utf-8")
+
 
 #detect language
 def translate_api(payload):
     conn = http.client.HTTPSConnection("google-translate1.p.rapidapi.com")
-    headers = {
-        'content-type': "application/x-www-form-urlencoded",
-        'accept-encoding': "application/gzip",
-        'x-rapidapi-key': "48a9d3acbemsh51e3d6835ad11bep1a253cjsne66f78646111",
-        'x-rapidapi-host': "google-translate1.p.rapidapi.com"
-        }
-    payload_final = "q="+str(payload)
+    headers = {'content-type': "application/x-www-form-urlencoded", 'accept-encoding': "application/gzip", 'x-rapidapi-key': "48a9d3acbemsh51e3d6835ad11bep1a253cjsne66f78646111", 'x-rapidapi-host': "google-translate1.p.rapidapi.com"}
+    payload_final = "q=" + str(payload)
     conn.request("POST", "/language/translate/v2/detect", payload_final, headers)
     res = conn.getresponse()
     data = res.read()
