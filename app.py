de64f2d24a53683e8699404843e3def6350a8762
SoniGarima <2018csb1089@iitrpr.ac.in>
2021-04-03 10:41:42 +0530

    added delete user functionality

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import mysql.connector
 from mysql.connector.constants import ClientFlag
 
 app = Flask(__name__)
-
+app.secret_key = ';\x01\x03A\x7f\x1d\xa8\x9e\x06\xf3\xf2m\x10"\xea\x99\x97\'\xf3\xc3\x0fQa\xbc'
 #### DATABASE CONNECTION PART######
 config = {'user': 'root', 'password': 'root', 'host': '35.192.77.35', 'database': 'ESB', 'client_flags': [
     ClientFlag.SSL], 'ssl_ca': 'ssl/server-ca.pem', 'ssl_cert': 'ssl/client-cert.pem', 'ssl_key': 'ssl/client-key.pem'}
@@ -71,6 +71,18 @@ def confirm_user(username):
         cursor = cnxn.cursor()
         cursor.execute('INSERT into Users(Username,UserPassword,UserRole,UserPriority) values(%s,%s,%s,%s)', (str(
             username), str(password), str(role), priority))
+        cnxn.commit()
+        return redirect(url_for('admin_dashboard'))
+    else:
+        return redirect(url_for("welcome_admin"))
+
+
+@app.route("/delete_user/<username>", methods=['GET', 'POST'])
+def delete_user(username):
+    if "username" in session and session["username"] == admin["username"]:
+        cursor = cnxn.cursor()
+        cursor.execute(
+            'DELETE from SignupConfirmation where username = %s', (str(username),))
         cnxn.commit()
         return redirect(url_for('admin_dashboard'))
     else:
