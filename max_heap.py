a5bf771c77ffe30e3bd654f926de94fe5b9fa7ba
Abhay Patil <abhay.patil2000@gmail.com>
2021-04-03 21:18:17 +0530

    code cleaning

--- max_heap.py
+++ max_heap.py
@@ -1,6 +1,5 @@
 # this is an implementation of maxheap which will be imported in priority_queue.py
 
-# from heapq import heappush, heappop, heapify
 from heapq_max import heappush_max, heappop_max, heapify_max
 
 
@@ -11,17 +10,14 @@ class SizeError(Exception):  # custsom exception for popping empty queue
 class max_heap:
     def __init__(self):
         self._heap = []
-        pass
 
     def push(self, x):
         heappush_max(self._heap, x)
-        pass
 
     def pop(self):
         if (self.size(self._heap) == 0):
             raise SizeError
         return heappop_max(self._heap)
-        pass
 
     def top(self):
         return self._heap[0]
@@ -31,7 +27,3 @@ class max_heap:
 
     def size(self):
         return len(self._heap)
-
-
-ls = [1, 2, 3]
-print(len(ls))
