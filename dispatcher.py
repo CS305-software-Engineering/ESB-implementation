c5a0d315f4b7edcdf5100a533edb327e66b4df33
sathwik-r <2018csb1081@iitrpr.ac.in>
2021-04-07 16:02:02 +0530

    misc

--- dispatcher.py
+++ dispatcher.py
@@ -1,26 +1,27 @@
-from multiprocessing.connection import Listener,Client
+from multiprocessing.connection import Listener, Client
 import json
 import time
 from socket import *
 import sys
 
-sock=socket()
+sock = socket()
 sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-# for instagram api
-listener_port = int(sys.argv[1])  #port on which processing module is sending data
-http_port = int(sys.argv[2])      #port on which HTTP server is listening
+#port on which processing module is sending data
+listener_port = int(sys.argv[1])
+#port on which HTTP server is listening
+http_port = int(sys.argv[2])
 
 listener = Listener(('localhost', listener_port), authkey=b'secret password')
 sender = Client(('localhost', http_port), authkey=b'secret password')
-running=True
+running = True
 while running:
-    con=listener.accept()
-    msg=con.recv()
+    con = listener.accept()
+    msg = con.recv()
     print(msg)
-    if msg=="terminate":
+    if msg == "terminate":
         con.close()
         sender.send(msg)
         sender.close()
-        running=False
+        running = False
         break
     sender.send(msg)
