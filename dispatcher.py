b050bc4aa3d26be5db92c937e46e874e9611d372
Abhay Patil <abhay.patil2000@gmail.com>
2021-04-30 23:06:38 +0530

    started database writup in dispatcher

--- dispatcher.py
+++ dispatcher.py
@@ -2,6 +2,38 @@ from multiprocessing.connection import Listener, Client
 import time
 import sys
 
+import os
+from dotenv import load_dotenv
+import mysql.connector
+from mysql.connector import Error
+
+load_dotenv()
+
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_DB = os.getenv('MYSQL_DB')
+
+
+def connect():
+    """ Connect to MySQL database """
+    conn = None
+    try:
+        conn = mysql.connector.connect(host='localhost',
+                                       database=str(MYSQL_DB),
+                                       user='root',
+                                       password=str(MYSQL_PASSWORD))
+        if conn.is_connected():
+            print('Connected to MySQL database')
+
+    except Error as e:
+        print(e)
+
+    finally:
+        if conn is not None and conn.is_connected():
+            conn.close()
+
+
+connect()
+
 time.sleep(2)
 # port on which processing module is sending data
 listener_port = int(sys.argv[1])
@@ -22,4 +54,4 @@ while running:
         running = False
         break
     # sender.send(msg)
-    print("to be put into db for communication")
+    print('write msg to database if msg is not terminate')
